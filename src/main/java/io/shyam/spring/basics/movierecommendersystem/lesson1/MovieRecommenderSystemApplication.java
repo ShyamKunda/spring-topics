dc5e08048376e50dd021a65ea5eb99175d9fc5d7
Kunda Shyam <shyam.k@shyam.k>
2023-02-15 17:57:42 +0530

    Dependency

---
+++ src/main/java/io/shyam/spring/basics/movierecommendersystem/lesson1/MovieRecommenderSystemApplication.java
@@ -0,0 +1,19 @@
+package io.shyam.spring.basics.movierecommendersystem.lesson1;
+
+import io.shyam.spring.basics.movierecommendersystem.lesson1.RecommenderImplementation;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class MovieRecommenderSystemApplication {
+
+	public static void main(String[] args) {
+
+		RecommenderImplementation recommender = new RecommenderImplementation();
+		String[] result = recommender.recommendMovies("Finding Dory");
+		System.out.println(Arrays.toString(result));
+	}
+
+}
---
+++ src/main/java/io/shyam/spring/basics/movierecommendersystem/lesson1/RecommenderImplementation.java
@@ -0,0 +1,13 @@
+package io.shyam.spring.basics.movierecommendersystem.lesson1;
+
+public class RecommenderImplementation {
+
+
+    public String[] recommendMovies (String movie) {
+        //use content based filter to find similar movies
+        ContentBasedFilter filter = new ContentBasedFilter();
+        String[] results = filter.getRecommendations(movie);
+        //return the results
+        return results;
+    }
+}
