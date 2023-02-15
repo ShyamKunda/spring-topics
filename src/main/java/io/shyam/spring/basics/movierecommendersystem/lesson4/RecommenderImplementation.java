430cb2c872396fc84a07b226fde98372d14b95b8
Kunda Shyam <shyam.k@shyam.k>
2023-02-15 18:01:31 +0530

    Autowiring by type - @primary

---
+++ src/main/java/io/shyam/spring/basics/movierecommendersystem/lesson4/RecommenderImplementation.java
@@ -0,0 +1,23 @@
+package io.shyam.spring.basics.movierecommendersystem.lesson4;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RecommenderImplementation {
+
+
+    @Autowired
+    private Filter filter;
+    public RecommenderImplementation(Filter filter) {
+        super();
+        this.filter = filter;
+    }
+    public String[] recommendMovies (String movie) {
+        //print the name of interface implementation being used
+        System.out.println("Name of the filter in use: " + filter + "\n");
+        String[] results = filter.getRecommendations(movie);
+        //return the results
+        return results;
+    }
+}
