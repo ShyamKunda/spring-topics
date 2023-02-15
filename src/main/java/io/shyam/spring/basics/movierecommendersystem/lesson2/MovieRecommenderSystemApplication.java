e748c21ade0c04b996974fbbc37469e1605caee0
Kunda Shyam <shyam.k@shyam.k>
2023-02-15 17:59:03 +0530

    Decoupling components

---
+++ src/main/java/io/shyam/spring/basics/movierecommendersystem/lesson2/MovieRecommenderSystemApplication.java
@@ -0,0 +1,17 @@
+package io.shyam.spring.basics.movierecommendersystem.lesson2;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class MovieRecommenderSystemApplication {
+
+	public static void main(String[] args) {
+
+		RecommenderImplementation recommender = new RecommenderImplementation(new CollaborativeFilter());
+		String[] result = recommender.recommendMovies("Finding Dory");
+		System.out.println(Arrays.toString(result));
+	}
+
+}
