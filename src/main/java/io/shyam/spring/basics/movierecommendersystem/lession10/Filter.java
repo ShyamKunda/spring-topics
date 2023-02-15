aebdc5c534e44e274c6f7404c82fadf063849b09
Kunda Shyam <shyam.k@shyam.k>
2023-02-15 20:58:27 +0530

    component scan

---
+++ src/main/java/io/shyam/spring/basics/movierecommendersystem/lession10/Filter.java
@@ -0,0 +1,7 @@
+package io.shyam.spring.basics.movierecommendersystem.lession10;
+
+public interface Filter {
+
+    public String[]
+    getRecommendations(String movie);
+}
--- src/main/java/io/shyam/spring/basics/movierecommendersystem/lesson9/MovieRecommenderSystemApplication.java
+++ src/main/java/io/shyam/spring/basics/movierecommendersystem/lesson9/MovieRecommenderSystemApplication.java
@@ -4,14 +4,28 @@ import io.shyam.spring.basics.movierecommendersystem.lesson8.CollaborativeFilter
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication
+//@ComponentScan(basePackages={"io.shyam.spring.basics.movierecommendersystem.lesson9",
+//		"io.shyam.spring.basics.movierecommendersystem.lession10"})
+@ComponentScan(basePackages = "io.shyam.spring.basics.movierecommendersystem.lession10")
+@ComponentScan(includeFilters = @ComponentScan.Filter (
+		type= FilterType.REGEX,
+		pattern="io.datajek.spring.basics.movierecommendersystem.lesson9.*"))
 public class MovieRecommenderSystemApplication {
 
 	public static void main(String[] args) {
 
 		//ApplicationContext manages the beans and dependencies
 		ApplicationContext appContext = SpringApplication.run(MovieRecommenderSystemApplication.class, args);
+
+
+		System.out.println("ContentBasedFilter bean found = " +
+				appContext.containsBean("contentBasedFilter"));
+		System.out.println("CollaborativeFilter bean found = " +
+				appContext.containsBean("collaborativeFilter"));
 
 		//Retrieve and print singleton bean from application context
 		ContentBasedFilter filter = appContext.getBean(ContentBasedFilter.class);
