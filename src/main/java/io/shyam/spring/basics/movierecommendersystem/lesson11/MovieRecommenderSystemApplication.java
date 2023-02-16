3fb693785b8b62146e2b3e9b9b2adb645311c2f6
Kunda Shyam <shyam.k@shyam.k>
2023-02-16 18:48:19 +0530

    protyped scoped beans

--- src/main/java/io/shyam/spring/basics/movierecommendersystem/lesson11/MovieRecommenderSystemApplication.java
+++ src/main/java/io/shyam/spring/basics/movierecommendersystem/lesson11/MovieRecommenderSystemApplication.java
@@ -10,23 +10,21 @@ import java.util.Arrays;
 public class MovieRecommenderSystemApplication {
 
 	public static void main(String[] args) {
-		//ApplicationContext manages the beans and dependencies
-		ApplicationContext appContext = SpringApplication.run(MovieRecommenderSystemApplication.class, args);
 
-		//use ApplicationContext to get recommender object
-		RecommenderImplementation recommender = appContext.getBean(RecommenderImplementation.class);
+		ApplicationContext appContext = SpringApplication.run(
+				MovieRecommenderSystemApplication.class, args);
 
-		System.out.println();
-		System.out.println();
+		//Retrieving singleton bean from application context
+		RecommenderImplementation recommender = appContext.getBean(
+				RecommenderImplementation.class);
 		System.out.println(recommender);
 
-		//call method to get recommendations
-		String[] result = recommender.recommendMovies("Finding Dory");
+		//Retrieving prototype bean from application context twice
+		Movie m1 = appContext.getBean(Movie.class);
+		System.out.println(m1);
 
-		//display results
-		System.out.println();
-		System.out.println();
-		System.out.println(Arrays.toString(result));
+		Movie m2 = appContext.getBean(Movie.class);
+		System.out.println(m2);
 
 	}
 
